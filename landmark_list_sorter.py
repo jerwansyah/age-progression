50b54d7149bff6d62bb73e33df658f1e03e5fb81
Jeane Erwansyah <blcktmyie123@gmail.com>
2022-12-19 18:20:17 +0700

    chore: fix landmark_list.txt

--- landmark_list_sorter.py
+++ landmark_list_sorter.py
@@ -1,19 +1,36 @@
 import pandas
 
+# # read the data from the file
+# data1 = pandas.read_csv('data/landmark_list/landmark_list_part1.txt', sep=' ', header=None)
+# data2 = pandas.read_csv('data/landmark_list/landmark_list_part2.txt', sep=' ', header=None)
+# data3 = pandas.read_csv('data/landmark_list/landmark_list_part3.txt', sep=' ', header=None)
+
+# # concatenate the data
+# data = pandas.concat([data1, data2, data3])
+
+# # add age column to data
+# data['age'] = data[0].apply(lambda x: int(x.split('_')[0]))
+
+# # sort the data
+# data.sort_values(by=['age'], inplace=True)
+# data.drop('age', axis=1, inplace=True)
+# data.drop(columns=[137], axis=1, inplace=True)
+
+# # save the data
+# data.to_csv('data/landmark_list/landmark_list.txt', sep=' ', header=False, index=False)
+
+#########
+
 # read the data from the file
-data1 = pandas.read_csv('data/landmark_list/landmark_list_part1.txt', sep=' ', header=None)
-data2 = pandas.read_csv('data/landmark_list/landmark_list_part2.txt', sep=' ', header=None)
-data3 = pandas.read_csv('data/landmark_list/landmark_list_part3.txt', sep=' ', header=None)
+data = pandas.read_csv('data/landmark_list/landmark_list.txt', sep=' ', header=None)
 
-# concatenate the data
-data = pandas.concat([data1, data2, data3])
+age_groups = [0, 2, 4, 7, 10, 13, 16, 20, 25, 35, 46, 57, 68]
 
-# add age column to data
 data['age'] = data[0].apply(lambda x: int(x.split('_')[0]))
+data['gender'] = data[0].apply(lambda x: x.split('_')[1])
+# data['gender'] = data['gender'].apply(lambda x: int(x))
+data['race'] = data[0].apply(lambda x: x.split('_')[2])
+# data['race'] = data['race'].apply(lambda x: int(x))
+# data['age_group'] = data['age'].apply(lambda x: )
 
-# sort the data
-data.sort_values(by=['age'], inplace=True)
-data.drop('age', axis=1, inplace=True)
-
-# save the data
-data.to_csv('data/landmark_list/landmark_list.txt', sep=' ', header=False, index=False)
+print(data.head())
