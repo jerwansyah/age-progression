1e6aeb5153208387450e9a5cc265a8e1c66569d8
Jeane Erwansyah <blcktmyie123@gmail.com>
2022-12-19 16:19:09 +0700

    feat: add feature detection and readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# age-progression
+# Age Progression
+
+## Libraries used
+
+- OpenCV
+- Dlib
+
+## How to run
+
+1. Run the following commands
+
+```sh
+curl http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2 --output .\data\shape_predictor_68_face_landmarks.dat.bz2
+mkdir data
+tar -xvzf .\data\shape_predictor_68_face_landmarks.dat.bz2 -C .\data\shape_predictor_68_face_landmarks.dat
+```
---
+++ feature_detection.py
@@ -0,0 +1,40 @@
+import dlib
+import cv2
+
+
+class FeatureDetection:
+    def __init__(self, predictor_path, detector_path):
+        self.predictor = dlib.shape_predictor('data/shape_predictor_68_face_landmarks.dat')
+        self.detector = dlib.get_frontal_face_detector()
+
+    def detect(self, image):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = self.detector(gray)
+        for face in faces:
+            shape = self.predictor(gray, face)
+            return shape
+
+    def save_points(self, shape, path, filename):
+        with open(path, 'a') as file:
+            file.write(filename)
+            for i in range(0, 68):
+                x = shape.part(i).x
+                y = shape.part(i).y
+                file.write(' ' + str(x) + ' ' + str(y))
+            file.write('\n')
+
+
+if __name__ == '__main__':
+    detector = FeatureDetection('shape_predictor_68_face_landmarks.dat', 'mmod_human_face_detector.dat')
+    image = cv2.imread('data/UTKFace/12_1_0_20170109204805155.jpg.chip.jpg')
+    shape = detector.detect(image)
+    detector.save_points(shape, 'data/points.txt', '12_1_0_20170109204805155')
+
+    for i in range(0, 68):
+        x = shape.part(i).x
+        y = shape.part(i).y
+        cv2.circle(image, (x, y), 2, (0, 255, 0), -1)
+
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
